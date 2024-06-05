c0e57369433138ceac9f1d3ab3b07fe42e4e906d
bojkomatias <bojko.matias@gmail.com>
2024-06-05 13:44:24 -0300

    Dialog primitive added

--- src/routes/docs/form/(form).tsx
+++ src/routes/docs/form/(form).tsx
@@ -1,54 +1,32 @@
 import { ArrowUpIcon } from "lucide-solid";
 import { createSignal } from "solid-js";
 import { Button } from "~/ui/button";
-import { Checkbox } from "~/ui/checkbox";
 import {
-  Description,
-  ErrorMesssage,
-  Field,
-  FieldGroup,
-  Fieldset,
-  Label,
-  Legend,
-} from "~/ui/fieldset";
-import { Input } from "~/ui/input";
-import { Text } from "~/ui/text";
-import { Textarea } from "~/ui/textarea";
+  Dialog,
+  DialogBody,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+} from "~/ui/dialog";
 
 export default function Home() {
-  const [checked, setChecked] = createSignal<boolean | "indeterminate">(true);
+  const [open, setOpen] = createSignal<boolean>(false);
 
   return (
     <div class="p-20">
-      <Button outline>
-        Hola <ArrowUpIcon data-slot="icon" />
+      <Button onClick={() => setOpen(true)}>
+        Open <ArrowUpIcon data-slot="icon" />
       </Button>
-      <form action="/orders" method="post" class="p-20">
-        {/* ... */}
-        <Fieldset>
-          <Legend>Shipping details</Legend>
-          <Text>Without this your odds of getting your order are low.</Text>
-          <FieldGroup>
-            <Field>
-              <Label for="sa">Street address</Label>
-              <Input name="street_address" id="sa" />
-            </Field>
-            <Field>
-              <Checkbox
-                checked={checked()}
-                onCheckedChange={(e) => setChecked(e.checked)}
-              >
-                This is garbaje
-              </Checkbox>
-              <Description>
-                If you have a tiger, we'd like to know about it.
-              </Description>
-              <ErrorMesssage>This is wrong</ErrorMesssage>
-            </Field>
-          </FieldGroup>
-        </Fieldset>
-        {/* ... */}
-      </form>
+      <Dialog open={open()} onOpenChange={() => setOpen(false)}>
+        <DialogContent>
+          <DialogTitle>Im a dialog</DialogTitle>
+          <DialogDescription>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut optio
+            accusantium!
+          </DialogDescription>
+          <DialogBody></DialogBody>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
--- src/ui/dialog.tsx
+++ src/ui/dialog.tsx
@@ -23,7 +23,7 @@ const sizes = {
   "4xl": "sm:max-w-4xl",
   "5xl": "sm:max-w-5xl",
 };
-const drawer = false;
+
 export function Dialog(
   props: {
     children: JSX.Element;
@@ -34,20 +34,19 @@ export function Dialog(
       <Portal>
         <ArkDialog.Backdrop
           class={clsx(
-            "fixed inset-0 z-20 flex w-screen justify-center overflow-y-auto px-2 py-2 focus:outline-0 sm:px-6 sm:py-8 lg:px-8 lg:py-16",
-            // animation
-            "data-[state=closed]:animate-[fade-out_0.2s_ease-in-out] data-[state=open]:animate-[fade-in_0.2s_ease-in-out]",
+            "pointer-events-none fixed inset-0 flex w-screen justify-center overflow-y-auto bg-zinc-950/25 px-2 py-2 focus:outline-0 sm:px-6 sm:py-8 lg:px-8 lg:py-16 dark:bg-zinc-950/50",
+            // Animation
+            "transition duration-300 data-[state='closed']:opacity-0 data-[state='open']:opacity-100",
           )}
         />
-        <ArkDialog.Positioner
-          class={clsx(
-            "fixed inset-x-0 flex w-screen overflow-y-auto pt-6 sm:pt-0",
-            !drawer
-              ? "bottom-0 justify-center sm:bottom-auto sm:top-24"
-              : "inset-y-0",
-          )}
-        >
-          {props.children}
+        <ArkDialog.Positioner class="fixed inset-0 w-screen overflow-y-auto pt-6 sm:pt-0">
+          <div
+            class={clsx(
+              "grid min-h-full grid-rows-[1fr_auto] justify-items-center sm:grid-rows-[1fr_auto_3fr] sm:p-4",
+            )}
+          >
+            {props.children}
+          </div>
         </ArkDialog.Positioner>
       </Portal>
     </ArkDialog.Root>
@@ -65,11 +64,7 @@ export const DialogContent = ({
       class={clsx(
         props.class,
         sizes[size],
-        "border-popover bg-card relative w-full rounded-xl rounded-b-none border p-8 shadow-lg sm:rounded-b-xl sm:p-6",
-        !drawer ? "" : "rounded-l-none",
-
-        // animation
-        "data-[state=closed]:animate-[fade-out_0.2s_ease-in-out] data-[state=open]:animate-[fade-in_0.2s_ease-in-out]",
+        "relative row-start-2 w-full min-w-0 rounded-t-3xl bg-white p-[--gutter] shadow-lg ring-1 ring-zinc-950/10 [--gutter:theme(spacing.8)] sm:mb-auto sm:rounded-2xl dark:bg-zinc-900 dark:ring-white/10 forced-colors:outline",
       )}
     >
       {props.children}
@@ -83,7 +78,7 @@ export function DialogTitle(props: DialogTitleProps) {
       {...props}
       class={clsx(
         props.class,
-        "text-foreground text-balance text-center text-base/6 font-semibold sm:text-wrap sm:text-left sm:text-sm/6",
+        "text-balance text-lg/6 font-semibold text-zinc-950 sm:text-base/6 dark:text-white",
       )}
     />
   );
@@ -93,13 +88,16 @@ export function DialogDescription(props: DialogDescriptionProps) {
   return (
     <ArkDialog.Description
       {...props}
-      class={clsx(props.class, "mt-2 text-pretty text-center sm:text-left")}
+      class={clsx(
+        props.class,
+        "mt-2 text-pretty text-zinc-800 dark:text-zinc-200",
+      )}
     />
   );
 }
 
 export function DialogBody(props: HTMLArkProps<"div">) {
-  return <div {...props} class={clsx(props.class, "mt-4")} />;
+  return <div {...props} class={clsx(props.class, "mt-6")} />;
 }
 
 export function DialogActions(props: HTMLArkProps<"div">) {
@@ -108,18 +106,8 @@ export function DialogActions(props: HTMLArkProps<"div">) {
       {...props}
       class={clsx(
         props.class,
-        "mt-6 flex flex-col-reverse items-center justify-end gap-3 *:w-full sm:mt-4 sm:flex-row sm:*:w-auto",
+        "mt-8 flex flex-col-reverse items-center justify-end gap-3 *:w-full sm:flex-row sm:*:w-auto",
       )}
     />
-  );
-}
-
-export function DialogClose() {
-  return (
-    <ArkDialog.CloseTrigger class="absolute right-3 top-3 justify-end">
-      <Button class="rounded-lg bg-transparent">
-        <X />
-      </Button>
-    </ArkDialog.CloseTrigger>
   );
 }
