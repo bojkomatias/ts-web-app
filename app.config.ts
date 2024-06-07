f730a4a8749b8190229f4d8fdff42e7eea34cb09
bojkomatias <bojko.matias@gmail.com>
2024-06-07 13:54:57 -0300

    cleanu p

--- app.config.ts
+++ app.config.ts
@@ -1,9 +1,3 @@
 import { defineConfig } from "@solidjs/start/config";
-// import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  // vite: {
-  //   // ssr: { external: ["@prisma/client"] },
-  //   // plugins: [tailwindcss()],
-  // },
-});
+export default defineConfig({});
